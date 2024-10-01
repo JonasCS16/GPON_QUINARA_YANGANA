377ef29c30b9ea2a03f9d5e433cf9066beebf1bb
JonasCS16 <jonascs1692@gmail.com>
2024-09-30 23:33:50 -0500

    Add files via upload

---
+++ data/DOMOS_10.js
@@ -0,0 +1,9 @@
+var json_DOMOS_10 = {
+"type": "FeatureCollection",
+"name": "DOMOS_10",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "ETIQUETA": null }, "geometry": { "type": "Point", "coordinates": [ -79.197597867294505, -4.3342736051324 ] } },
+{ "type": "Feature", "properties": { "id": null, "ETIQUETA": null }, "geometry": { "type": "Point", "coordinates": [ -79.176493267163977, -4.364350902011704 ] } }
+]
+}
---
+++ data/POSTES_CNT_1.js
@@ -0,0 +1,12 @@
+var json_POSTES_CNT_1 = {
+"type": "FeatureCollection",
+"name": "POSTES_CNT_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "ETIQUETA": "CNT42" }, "geometry": { "type": "Point", "coordinates": [ -79.219684012986832, -4.295590421644262 ] } },
+{ "type": "Feature", "properties": { "id": null, "ETIQUETA": "CNT90" }, "geometry": { "type": "Point", "coordinates": [ -79.207118445757288, -4.310898781914554 ] } },
+{ "type": "Feature", "properties": { "id": null, "ETIQUETA": "CNT124" }, "geometry": { "type": "Point", "coordinates": [ -79.210354986204393, -4.31633728218539 ] } },
+{ "type": "Feature", "properties": { "id": null, "ETIQUETA": "CNT141" }, "geometry": { "type": "Point", "coordinates": [ -79.197597867294505, -4.3342736051324 ] } },
+{ "type": "Feature", "properties": { "id": null, "ETIQUETA": "CNT169" }, "geometry": { "type": "Point", "coordinates": [ -79.19096612790203, -4.348071203406587 ] } }
+]
+}
