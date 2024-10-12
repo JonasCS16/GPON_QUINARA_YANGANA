a91816811455308cfec647df33feff953c898dee
JonasCS16 <jonascs1692@gmail.com>
2024-10-11 20:56:18 -0500

    Add files via upload

--- data/DOMOS_8.js
+++ data/DOMOS_8.js
@@ -5,6 +5,8 @@ var json_DOMOS_8 = {
 "features": [
 { "type": "Feature", "properties": { "id": null, "ETIQUETA": "H2D2" }, "geometry": { "type": "Point", "coordinates": [ -79.197597867294505, -4.3342736051324 ] } },
 { "type": "Feature", "properties": { "id": null, "ETIQUETA": "H2D3" }, "geometry": { "type": "Point", "coordinates": [ -79.176493267163977, -4.364350902011704 ] } },
-{ "type": "Feature", "properties": { "id": null, "ETIQUETA": "H2D1" }, "geometry": { "type": "Point", "coordinates": [ -79.205022161637402, -4.321255129904627 ] } }
+{ "type": "Feature", "properties": { "id": null, "ETIQUETA": "H2D1" }, "geometry": { "type": "Point", "coordinates": [ -79.205022161637402, -4.321255129904627 ] } },
+{ "type": "Feature", "properties": { "id": null, "ETIQUETA": null }, "geometry": { "type": "Point", "coordinates": [ -79.236295750494165, -4.318595138506289 ] } },
+{ "type": "Feature", "properties": { "id": null, "ETIQUETA": null }, "geometry": { "type": "Point", "coordinates": [ -79.221127987708144, -4.329194750065931 ] } }
 ]
 }
--- data/SECTOR_11.js
+++ data/SECTOR_11.js
@@ -7,6 +7,14 @@ var json_SECTOR_11 = {
 { "type": "Feature", "properties": { "id": null, "NOMBRE": "Chalaca" }, "geometry": { "type": "Point", "coordinates": [ -79.18138752177758, -4.358153759646552 ] } },
 { "type": "Feature", "properties": { "id": null, "NOMBRE": "Suro" }, "geometry": { "type": "Point", "coordinates": [ -79.190619913222932, -4.340097370601336 ] } },
 { "type": "Feature", "properties": { "id": null, "NOMBRE": "Masanamaca" }, "geometry": { "type": "Point", "coordinates": [ -79.204887470328771, -4.320149286666266 ] } },
-{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Patinuma" }, "geometry": { "type": "Point", "coordinates": [ -79.18747847495608, -4.349965419291594 ] } }
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Patinuma" }, "geometry": { "type": "Point", "coordinates": [ -79.18747847495608, -4.349965419291594 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Quinara" }, "geometry": { "type": "Point", "coordinates": [ -79.234604294809429, -4.319589867578133 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Guaguanga" }, "geometry": { "type": "Point", "coordinates": [ -79.2405054087682, -4.313409848571212 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "La Elvira" }, "geometry": { "type": "Point", "coordinates": [ -79.210384544253259, -4.350398379763932 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Marcopamba" }, "geometry": { "type": "Point", "coordinates": [ -79.200325502541517, -4.354394721010737 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Comunidades" }, "geometry": { "type": "Point", "coordinates": [ -79.224135354292216, -4.336619388307911 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "San Gabriel" }, "geometry": { "type": "Point", "coordinates": [ -79.221721059257717, -4.353367588538255 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Tumianuma" }, "geometry": { "type": "Point", "coordinates": [ -79.261625913562625, -4.30519764754894 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "El Sauce" }, "geometry": { "type": "Point", "coordinates": [ -79.262264873465369, -4.301627155793474 ] } }
 ]
 }
