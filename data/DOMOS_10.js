80222b6b2fd0581508e298006c32ded10c5ba1ae
JonasCS16 <jonascs1692@gmail.com>
2024-10-17 20:55:49 -0500

    Add files via upload

--- data/DOMOS_10.js
+++ data/DOMOS_10.js
@@ -3,7 +3,11 @@ var json_DOMOS_10 = {
 "name": "DOMOS_10",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "id": null, "ETIQUETA": null }, "geometry": { "type": "Point", "coordinates": [ -79.197597867294505, -4.3342736051324 ] } },
-{ "type": "Feature", "properties": { "id": null, "ETIQUETA": null }, "geometry": { "type": "Point", "coordinates": [ -79.176493267163977, -4.364350902011704 ] } }
+{ "type": "Feature", "properties": { "id": null, "ETIQUETA": "H2D2" }, "geometry": { "type": "Point", "coordinates": [ -79.197597867294505, -4.3342736051324 ] } },
+{ "type": "Feature", "properties": { "id": null, "ETIQUETA": "H2D3" }, "geometry": { "type": "Point", "coordinates": [ -79.176493267163977, -4.364350902011704 ] } },
+{ "type": "Feature", "properties": { "id": null, "ETIQUETA": "H2D1" }, "geometry": { "type": "Point", "coordinates": [ -79.205022161637402, -4.321255129904627 ] } },
+{ "type": "Feature", "properties": { "id": null, "ETIQUETA": null }, "geometry": { "type": "Point", "coordinates": [ -79.236295750494165, -4.318595138506289 ] } },
+{ "type": "Feature", "properties": { "id": null, "ETIQUETA": null }, "geometry": { "type": "Point", "coordinates": [ -79.240342309104193, -4.314279954352433 ] } },
+{ "type": "Feature", "properties": { "id": null, "ETIQUETA": null }, "geometry": { "type": "Point", "coordinates": [ -79.257544743348092, -4.303755998993715 ] } }
 ]
 }
